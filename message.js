e17527ac0c179333b1d7e16ab972c81b8a49b5dd
Burhan <burhaniddin123@gamil.com>
2023-01-07 11:39:17 +0300

    Added another new

---
+++ message.js
@@ -0,0 +1,9 @@
+
+
+const message=()=>{
+    const name="Mustafa";
+    const age=25;
+    return name+" is "+ age+" years old";
+};
+
+export default message;
---
+++ person.js
@@ -0,0 +1,8 @@
+
+
+//export const name="Burhan";
+//export const age=23;
+
+const name="Burhan";
+const age=34;
+export {name, age};
